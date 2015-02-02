f347558e0d0998d200edcb701a918df3569702d0
John Morris <John@Johns-MacBook-Pro-2.local>
2015-02-02 15:34:42 +1000

    fixed Locations page

--- src/shared/javascripts/generic-fittext.js
+++ src/shared/javascripts/generic-fittext.js
@@ -3,3 +3,6 @@ $('.js-headline').css({
 });
 $('.js-headline').fitText(0.525);
 $('.js-fy16').fitText(0.20);
+$('.js-location').fitText(0.525);
+$('.js-date').fitText(1.1);
+$('.js-subtitle').fitText(1.5);
