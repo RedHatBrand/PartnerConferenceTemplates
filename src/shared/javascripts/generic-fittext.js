8e499cf6d66f4202afff35a6161455614a2ed255
John Morris <John@Johns-MacBook-Pro-2.local>
2015-02-03 08:54:56 +1000

    Fixed Location Again

--- src/shared/javascripts/generic-fittext.js
+++ src/shared/javascripts/generic-fittext.js
@@ -1,8 +1,4 @@
 $('.js-headline').css({
   'word-spacing': $('.js-headline').width()
 });
-$('.js-headline').fitText(0.525);
-$('.js-fy16').fitText(0.20);
-$('.js-location').fitText(0.525);
-$('.js-date').fitText(1.1);
-$('.js-subtitle').fitText(1.5);
+$('.js-headline').fitText(0.5);
