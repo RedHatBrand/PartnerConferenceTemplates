e5daeff3e1eb51f1ead2a5e241a7d0de9fc6c33b
Beau Hankins <beau.j.hankins@gmail.com>
2015-02-04 08:33:30 +1000

    Artwork mustache data added for all templates

--- src/shared/javascripts/artwork.js
+++ src/shared/javascripts/artwork.js
@@ -206,7 +206,21 @@
   }
 
   drawArtwork();
-
-
-
+  
+  // Media Queries
+  
+  if (matchMedia) {
+	var mq = window.matchMedia("(min-width: 500px)");
+	mq.addListener(WidthChange);
+	WidthChange(mq);
+    
+  function WidthChange(mq) {
+    if (mq.matches) {
+		  // window width is at least 500px
+    } else {
+		  // window width is less than 500px
+    }
+  }
+}
+  
 })(window);
